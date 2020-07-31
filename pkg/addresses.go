46fbd929cab890ea5c4dbc777d1d233d885e3273
ltacker <lucas.bertrand.22@gmail.com>
2020-07-31 15:21:32 +0200

    New contracts

--- pkg/addresses.go
+++ pkg/addresses.go
@@ -6,7 +6,7 @@ func boardHandlerAddress() string {
 	network := os.Getenv("WEB3_NETWORK")
 
 	if network == "rinkeby" {
-		return "0x5523F902F6dE92835d3Aa20C265503DD9862925A"
+		return "0x85127533B8644a37798763864bE73a4E123710F8"
 	} else {
 		return "0xf036e42b361f1B23E6aCe6Edbc6247339857f328"
 	}
@@ -16,7 +16,7 @@ func ethboardsAddress() string {
 	network := os.Getenv("WEB3_NETWORK")
 
 	if network == "rinkeby" {
-		return "0xF13eB11ed35F1Bd949470bF8E2DF67f711816348"
+		return "0x863abC9ED306dC45bF6ad2cF50666000487f750E"
 	} else {
 		return "0xF6f34C20D4Dfc084f649101C38855754bC273bce"
 	}
